0f1465e00685846a8e51565dc86d65618d4cd521
Khalid Kamil <khalidkamil28@gmail.com>
2023-10-13 10:58:01 +0100

    Integrating Core Image with SwiftUI

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -1,29 +1,54 @@
-//  Showing multiple options with confirmationDialog()
+//  Integrating Core Image with SwiftUI
 
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
-    @State private var showingConfirmation = false
-    @State private var backgroundColor = Color.white
+    @State private var image: Image?
 
     var body: some View {
-        Text("Hello, World!")
-            .frame(width: 300, height: 300)
-            .background(backgroundColor)
-            .onTapGesture {
-                showingConfirmation = true
-            }
-            .confirmationDialog("Change background", isPresented: $showingConfirmation) {
-                Button("Red") { backgroundColor = .red }
-                Button("Green") { backgroundColor = .green }
-                Button("Blue") { backgroundColor = .blue }
-                Button("Cancel", role: .cancel) { }
-            } message: {
-                Text("Select a new color")
-            }
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+        }
+        .onAppear(perform: loadImage)
     }
 }
 
 #Preview {
     ContentView()
+}
+
+extension ContentView {
+    func loadImage() {
+        guard let inputImage = UIImage(named: "Example") else { return }
+        let beginImage = CIImage(image: inputImage)
+
+        let context = CIContext()
+        let currentFilter = CIFilter.twirlDistortion()
+        currentFilter.inputImage = beginImage
+        
+        let amount = 1.0
+
+        let inputKeys = currentFilter.inputKeys
+
+        if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(amount, forKey: kCIInputIntensityKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey) }
+        currentFilter.radius = 1000
+        currentFilter.center = CGPoint(x: inputImage.size.width/2, y: inputImage.size.height / 2)
+
+        // get a CIImage from our filter or exit if that fails
+        guard let outputImage = currentFilter.outputImage else { return }
+
+        // attempt to get a CGImage from our CIImage
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            // convert that to a UIImage
+            let uiImage = UIImage(cgImage: cgimg)
+            // and convert that to a SwiftUI image
+            image = Image(uiImage: uiImage)
+        }
+    }
 }
